034f9ec9c58237ca96d32d9de3fd3ca779cbf2ea
Martin Dennhardt <martin@dennhardt.de>
2023-06-17 12:40:22 +0200

    Configure telescope build

--- init.lua
+++ init.lua
@@ -174,11 +174,9 @@ require('lazy').setup({
   -- requirements installed.
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    cond = function ()
+      return vim.fn.executable 'cmake' == 1
     end,
   },
 
